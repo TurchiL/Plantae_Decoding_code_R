d986b519a9bd82f653208860b0e5976f6fe76256
TurchiL <78414816+TurchiL@users.noreply.github.com>
2023-06-11 12:24:41 +0200

    Updated heatmap and PCA
    Updated code to make sure it works now.

--- R_guide_code.r
+++ R_guide_code.r
@@ -2,7 +2,7 @@
 
 ## Upload your data -- using iris dataset as example
 ## iris csv dataset downloaded from https://gist.github.com/netj/8836201
-input_file <- "C:/Users/lt258356.INTRA/Downloads/iris_data.csv" # this is called a "variable"
+input_file <- "your_input_directory/iris_data.csv" # this is called a "variable"
 
 ## read your input data as a data frame (tabular structure)
 df <- read.table(input_file,sep=",",header=TRUE) # sep specifies the "separator" (in this case commas), header = TRUE to consider the first line as the "title" of each column
@@ -19,14 +19,14 @@ library(ggplot2)
 ggplot(df, aes(x = sepal.length)) +
   geom_histogram(binwidth = 0.2, fill = "skyblue", color = "black") +
   labs(title = "Histogram of Sepal Length", x = "Sepal Length", y = "Count")
-ggsave("C:/Users/lt258356.INTRA/Downloads/sepal_length_histogram.png") # save plot
+ggsave("your_input_directory/sepal_length_histogram.png") # save plot
 
 
 ## Density plot of sepal length
 ggplot(df, aes(x = sepal.length)) +
   geom_density(fill = "skyblue", color = "black", alpha=.7) +
   labs(title = "Density plot of Sepal Length", x = "Sepal Length", y = "Count")
-ggsave("C:/Users/lt258356.INTRA/Downloads/sepal_length_kernel_density.png") # save plot
+ggsave("your_input_directory/sepal_length_kernel_density.png") # save plot
 
 
 ## Scatter plot with linear regression line of sepal vs petal length
@@ -34,21 +34,21 @@ ggplot(df, aes(x = sepal.length, y = petal.length)) + #df$sepal.length, df$petal
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
   labs(title = "Petal vs Sepal Length", x = "Sepal Length", y = "Petal Length")
-ggsave("C:/Users/lt258356.INTRA/Downloads/linear_regression.png") # save plot
+ggsave("your_input_directory/linear_regression.png") # save plot
 
 
 ## Boxplot of petal length by species
 ggplot(df, aes(x = variety, y = petal.length, fill=variety)) +
   geom_boxplot() +
   labs(title = "Box Plot of Petal Length by Species", x = "Species", y = "Petal Length")
-ggsave("C:/Users/lt258356.INTRA/Downloads/petal_length_by_species.png") # save plot
+ggsave("your_input_directory/petal_length_by_species.png") # save plot
 
 
 ## Violin plot of petal width by species
 ggplot(df, aes(x = variety, y = petal.width, fill = variety)) +
   geom_violin() +
   labs(title = "Petal Width by Species", x = "Species", y = "Petal Width")
-ggsave("C:/Users/lt258356.INTRA/Downloads/violin_petal_width_by_species.png") # save plot
+ggsave("your_input_directory/violin_petal_width_by_species.png") # save plot
 
 
 ## Violin + data points + boxplot
@@ -57,7 +57,7 @@ ggplot(df, aes(x = variety, y = petal.width, fill = variety)) +
   geom_jitter(width=0.15,height=0, col="gray20",alpha=0.3)+
   geom_boxplot(fill="white",width=0.1)+ # add boxplot over violins
   labs(title = "Petal Width by Species", x = "Species", y = "Petal Width")
-ggsave("C:/Users/lt258356.INTRA/Downloads/violin_jitter_boxplot_petal_width_by_species.png") # save plot
+ggsave("your_input_directory/violin_jitter_boxplot_petal_width_by_species.png") # save plot
 
 
 ## Heatmaps
@@ -65,12 +65,18 @@ install.packages("pheatmap")
 library(pheatmap)
 
 #Data processing for Heatmap
-Heatmap_data <- df[apply(df, 1, function(x) sd(x) != 0),]
+Heatmap_data <- df[,1:4]
+head(Heatmap_data)
+row.names(Heatmap_data) <- row.names(df)
 
 #Heatmap
-Heatmap <- pheatmap(Heatmap_data, scale="row")
-
-
+png("your_input_directory/heatmap.png",res=300,width=7,height=5,units = "in")
+pheatmap(Heatmap_data,
+         scale = "column", # plot values as they are
+         # clustering_method = "average", # average linkage
+         annotation_row = df[, 5, drop = FALSE], # the 5th column as color bar
+         show_rownames = FALSE)
+dev.off()
 
 
 
@@ -105,12 +111,19 @@ summary(Two_way_anova) #Summarize the two-way ANOVA results
 
 
 ## PCA
-# Load the "PCAtools" package
-install.packages("PCAtools")
-library(PCAtools)
+install.packages("FactoMineR") # for PCA
+install.packages("factoextra") # for PCA visualization
+
+library(FactoMineR)
+library(factoextra)
 
 # Run PCA
-PCA <- pca(df, scale= TRUE, removeVar = 0.1)
+pca <- PCA(data, graph = FALSE)
 
-# Visualize PCA
-biplot(PCA)
+# Visualize PCA results
+fviz_pca_biplot(pca, 
+                col.ind = df$variety,
+                addEllipses = TRUE, label = "var",
+                col.var = "black", repel = TRUE,
+                legend.title = "Species") 
+ggsave("your_input_directory/PCA_plot.png",dpi=300,bg="white")
